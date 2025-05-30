1b8f1b0b2b6a44c5a175c48a674ea4e5bdac2a72
Ahmedgito <mamooabid1@gmail.com>
2025-05-30 22:50:25 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DemoHeroGeometric from "@/components/demo";
+import BentoGridDemo from "@/components/feature";
+import './App.css';
+function App() {
+  return (
+    <>
+      <DemoHeroGeometric />
+      <div className="dark bg-black min-h-screen"> <BentoGridDemo/></div>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/demo.jsx
@@ -0,0 +1,11 @@
+import  HeroGeometric from "@/components/ui/shape-landing-hero"
+
+
+function DemoHeroGeometric() {
+    return <HeroGeometric badge="Rovamo"
+            title1 = "Elevate Your"
+            title2 = "Digital Vision" />
+}
+
+export default DemoHeroGeometric
+
